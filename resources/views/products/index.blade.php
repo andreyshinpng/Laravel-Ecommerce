387f7acc591ec2ac7a2eb50534f677f563c16357
Andrey Shin <andreyshinpng@yandex.kz>
2022-11-19 21:26:49 +0500

    added some frontend and output of products from ORM

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main')
+
+@section('header')
+    <div class="navbar_left">
+        <a href="/">Home page</a>
+    </div>
+    <div class="navbar_right">
+        <a href="#">Cart</a>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+    </div>
+@endsection
+
+@section('content')
+    <div class="product_wrapper">
+        @foreach ($products as $product)
+            <div class="product">
+                <h3>{{ $product->title }}</h3>
+                <p>Description: {{ $product->description }}</p>
+                <p>Price: {{ $product->price }}</p>
+            </div>
+        @endforeach
+    </div>
+@endsection
