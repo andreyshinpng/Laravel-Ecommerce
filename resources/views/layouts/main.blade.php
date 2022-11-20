6e2ee1dca8a38fe80b4f4693f32bedea7fc64501
Andrey Shin <andreyshinpng@yandex.kz>
2022-11-20 12:04:26 +0500

    changed frontend

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,22 +6,55 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/main.css">
-    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>E-Commerce</title>
 </head>
 <body>
 
 <header>
     <div class="container">
         <div class="navbar">
-            @yield('header')
+            <div class="navbar_left">
+                <a href="/"><b>E-Commerce</b></a>
+            </div>
+            <div class="navbar_right">
+                <a href="#">Cart</a>
+                <a href="#">Login</a>
+                <a href="#" class="btn btn-primary">Register</a>
+            </div>
         </div>
     </div>
 </header>
 
-<section class="content_section">
+<section id="content_section">
     <div class="container">
         <div class="content">
-            @yield('content')
+            <div class="row" id="product_row">
+                @for ($i = 1; $i <= 5; $i++)
+                    <div class="col" id="product">
+                        <h4>Test product</h4>
+                        <p id="product_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lobortis posuere.</p>
+                        <p style="font-weight: bold">Price: $1.23</p>
+                        <button class="btn btn-success mt-2">Add to Cart</button>
+                    </div>
+                @endfor
+            </div>
+            <div class="row" id="product_row">
+                @for ($i = 1; $i <= 5; $i++)
+                    <div class="col" id="product">
+                        <h4>Test product</h4>
+                        <p id="product_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lobortis posuere.</p>
+                        <p style="font-weight: bold">Price: $1.23</p>
+                        <button class="btn btn-success mt-2">Add to Cart</button>
+                    </div>
+                @endfor
+            </div>
+            <div id="pagination" class="mt-3">
+                <b>Pages:</b>
+                <a href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+            </div>
         </div>
     </div>
 </section>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.main')
 
-@section('header')
-    <div class="navbar_left">
-        <a href="/">Home page</a>
-    </div>
-    <div class="navbar_right">
-        <a href="#">Cart</a>
-        <a href="#">Login</a>
-        <a href="#">Register</a>
-    </div>
-@endsection
-
 @section('content')
     <div class="product_wrapper">
         @foreach ($products as $product)
